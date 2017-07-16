35ad3aa7a2d0aa66d60acf9c7dd40adecbc2545b
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-07-16 15:31:31 +0530

    Adding chat bot page powred by api.ai

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule }   from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { IndoorMapComponent } from './indoor-map/indoor-map.component';
+import { ChatBotComponent } from './chat-bot/chat-bot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    IndoorMapComponent
+    IndoorMapComponent,
+    ChatBotComponent
   ],
   imports: [
     NgbModule.forRoot(),
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot([
       {
         path: 'home',
@@ -23,6 +27,9 @@ import { IndoorMapComponent } from './indoor-map/indoor-map.component';
       }, {
         path: 'indoor-map',
         component: IndoorMapComponent
+      },{
+        path: 'chat-bot',
+        component: ChatBotComponent
       }, {
         path: '',
         redirectTo: '/home',
---
+++ src/app/chat-bot/chat-bot.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {ApiAiClient} from "api-ai-javascript";
+
+const client = new ApiAiClient({accessToken: 'b71bf0851f6f41f8b1728e20c7946c25'})
+
+@Component({
+  selector: 'app-chat-bot',
+  templateUrl: './chat-bot.component.html',
+  styleUrls: ['./chat-bot.component.css']
+})
+export class ChatBotComponent implements OnInit {
+  messages : [any];
+  newMessage: any;
+
+
+  constructor() {
+    let _self = this;
+    this.newMessage = {content: 'Hello!'};
+    this.messages = [{content: 'Hello!'}];
+    this.sendMessage(this.newMessage, (response) => {
+      console.log(response);
+      _self.messages.push({content: response.result.fulfillment.speech});
+    });
+   }
+   sendMessage(message, cb) {
+   client.textRequest(message.content)
+       .then((response) => {
+        cb(response);
+       })
+       .catch((error) => {
+         console.error(error);
+       });
+   }
+   send(message: any): void {
+   let _self = this;
+   this.sendMessage(this.newMessage, (response) => {
+     console.log(response);
+      var result = {isNavigation: false, content: ''};
+      result.isNavigation = response.result.action === 'navigation';
+      result.content= response.result.fulfillment.speech;
+     _self.messages.push(result);
+   });
+   }
+
+  ngOnInit() {
+  }
+
+}
