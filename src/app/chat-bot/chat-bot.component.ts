cba34f08bba2dd649e3a43a99a680cb119ca1aec
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-07-17 15:41:02 +0530

    Adding whole username for image instead of Initial

--- src/app/chat-bot/chat-bot.component.ts
+++ src/app/chat-bot/chat-bot.component.ts
@@ -14,14 +14,14 @@ export class ChatBotComponent implements OnInit {
 
   constructor() {
     let _self = this;
-    this.newMessage = {content: 'Hello!'};
+    this.newMessage = {content: 'Hi Donna'};
     this.sendMessage(this.newMessage, (response) => {
       _self.addMessage(response, 'Donna');
     });
    }
    addMessage(response, user) {
      console.log(response);
-      var result = {isNavigation: false, content: '', isSelf: false, user : user, timestamp : new Date(), symbol: 'D'};
+      var result = {isNavigation: false, content: '', isSelf: false, user : user, timestamp : new Date()};
       result.isNavigation = response.result.action === 'navigation';
       result.content= response.result.fulfillment.speech;
       if(this.messages) {
@@ -31,7 +31,7 @@ export class ChatBotComponent implements OnInit {
       }
    }
    sendMessage(message, cb) {
-    let cMsg = {isNavigation: false, content: message.content, isSelf: true, user : 'Me', timestamp : new Date(), symbol: 'ME'};
+    let cMsg = {isNavigation: false, content: message.content, isSelf: true, user : 'You', timestamp : new Date()};
      if(this.messages) {
       this.messages.push(cMsg);
      } else {
