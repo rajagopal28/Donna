41a8b27f7d37be739b34b37f1ecbaad133edc591
Rajagopal M <Rajagopal M>
2017-07-22 21:17:45 +0530

    Adding tabbed view in app component

--- nativescript/app/app.component.ts
+++ nativescript/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from "@angular/core";
+import { StackLayout } from "ui/layouts/stack-layout";
+
+import { TabView, SelectedIndexChangedEventData, TabViewItem } from "ui/tab-view";
+
+
+export class DataItem {
+    constructor(public itemDesc: string) {}
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,14 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
     title = 'Donna';
+    public items: Array<DataItem>;
+    public tabSelectedIndex: number;
+
+    constructor() {
+        this.tabSelectedIndex = 1;
+        this.items = new Array<DataItem>();
+        for (let i = 0; i < 5; i++) {
+            this.items.push(new DataItem("item " + i));
+        }
+    }
 }
