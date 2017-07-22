414173e2f08942d9f65e0dbec7ac90d626ed4241
Rajagopal M <Rajagopal M>
2017-07-22 22:50:23 +0530

    Adding custom home content inile in tab view

--- nativescript/app/app.component.ts
+++ nativescript/app/app.component.ts
@@ -15,6 +15,8 @@ export class DataItem {
 })
 export class AppComponent {
     title = 'Donna';
+    public htmlString: string;
+
     public items: Array<DataItem>;
     public tabSelectedIndex: number;
 
@@ -22,7 +24,9 @@ export class AppComponent {
         this.tabSelectedIndex = 1;
         this.items = new Array<DataItem>();
         for (let i = 0; i < 5; i++) {
-            this.items.push(new DataItem("item " + i));
+            this.items.push(new DataItem("item " + i*10));
         }
+        console.log('Varuthaa...');
+        this.htmlString = '<p> We are presenting <b>Donna !! Your personalised office assistant!</b>The primary responsibility of Donna is to help you internally navigate in you huge corporate office and get you things done. Apart from internal navigation, she can also help you more productive, healthy and smart in a lot of ways. The proposed system can be integrated with public announcement systems, office update systems, food provider systems, business emails and meeting management systems etc, to help you at your work.</p>';
     }
 }
