13ae5d3c2dfbb022c24e5a19938676fdcedfaa56
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-14 16:52:04 +0530

    Adding AngularGoogleMaps Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule }   from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { IndoorMapComponent } from './indoor-map/indoor-map.component';
 import { ChatBotComponent } from './chat-bot/chat-bot.component';
+import { GoogleIndoorComponent } from './google-indoor/google-indoor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     IndoorMapComponent,
-    ChatBotComponent
+    ChatBotComponent,
+    GoogleIndoorComponent
   ],
   imports: [
     NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: '<YourAPIKey>'
+    }),
     RouterModule.forRoot([
       {
         path: 'home',
@@ -30,6 +36,9 @@ import { ChatBotComponent } from './chat-bot/chat-bot.component';
       },{
         path: 'chat-bot',
         component: ChatBotComponent
+      },{
+        path: 'google-indoor',
+        component: GoogleIndoorComponent
       }, {
         path: '',
         redirectTo: '/home',
