b26fd3271a3c74589ac68d2ac8f7f41c75921d5e
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-25 15:18:03 +0530

    Adding http module in nativescript

--- nativescript/app/app.module.ts
+++ nativescript/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptHttpModule } from "nativescript-angular/http";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from './home/home.component';
@@ -10,7 +11,7 @@ import { GoogleIndoorComponent } from './google-indoor/google-indoor.component';
 @NgModule({
   declarations: [AppComponent, HomeComponent, ChatBotComponent, IndoorMapComponent, GoogleIndoorComponent],
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule],
+  imports: [NativeScriptModule, NativeScriptHttpModule],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
