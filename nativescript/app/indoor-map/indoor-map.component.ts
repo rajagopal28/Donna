98d970bafb4b31fa993b51d5481bb3ca5a9ebb8e
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-26 10:59:29 +0530

    Updating indoor map component with bit refactoring on the api call

--- nativescript/app/indoor-map/indoor-map.component.ts
+++ nativescript/app/indoor-map/indoor-map.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
-import { WebView, LoadEventData } from "ui/web-view";
+import { Color } from "tns-core-modules/color";
 
 import {registerElement} from 'nativescript-angular/element-registry';
+
+import { Polyline, Marker, Position } from "nativescript-google-maps-sdk";
 
 var mapsModule = require("nativescript-google-maps-sdk");
 
@@ -15,11 +17,16 @@ import { IndoorLocationDataService } from '../services/indoor-location-data.serv
 export class IndoorMapComponent implements OnInit {
   @ViewChild("MapView2") mapView: ElementRef;
 
-    lat: number = -34.5332878;
-    lng: number = 138.9511826;
+    lat: number = 12.9051301;
+    lng: number = 80.2260227;
+    routeText: string = '';
     zoom: number = 17;
     bearing:number = 0;
     tilt:number = 0;
+    p: any = {
+      "origin" : "12.9033372,80.2252904",
+      "destination" : "12.9094935,80.2250325"
+    };
     constructor(
      private hotSpotService: IndoorLocationDataService) {}
 
@@ -30,7 +37,7 @@ export class IndoorMapComponent implements OnInit {
     }
     getDirections(arg, cb) {
       console.log('getting directions', arg);
-      this.hotSpotService.getDirectionWayPointsAndLoadPolyLineOptions({}, (result)=> {
+      this.hotSpotService.getDirectionWayPointsAndLoadPolyLineOptions(arg, (result)=> {
         console.log('inside promise');
         cb(result);
       });
@@ -51,23 +58,36 @@ export class IndoorMapComponent implements OnInit {
 
       // setup marker
       console.log('setting marker on location');
-      var marker = new mapsModule.Marker();
-      marker.position = mapsModule.Position.positionFromLatLng(this.lat, this.lng);
-      marker.title = "Barossa Valley, Tanunda SA 5352";
-      marker.snippet = "Australia";
-      marker.userData = { index : 1};
+      var marker = this.getMarkerFrom(this.lat, this.lng, 'Accenture', 'Shollinganallur');
       mapView.addMarker(marker);
-      this.getDirections({}, (result) => {
+      // this.hotSpotService.getRouteText(this.p, (response) => {
+      //   console.log('route text');
+      //   this.routeText = response;
+      // });
+      this.getDirections(this.p, (result: Polyline) => {
         try {
-            if(result.android) {
-                console.log('got android and setting..');
-                mapView.gMap.addPolyline(result.android);
+            if(result) {
+                console.log('got android and setting.. Polyline');
+                mapView.addPolyline(result);
+                let src = this.getMarkerFrom(12.9094935,80.2250325, 'Some place 1', 'Some snipp1');
+                let dst = this.getMarkerFrom(12.9033372,80.2252904, 'some place 2','Some snipp2');
+                mapView.addMarker(src);
+                mapView.addMarker(dst);
+                console.log('Adding src dst marker');
             }
         } catch(e) {
           console.error(e);
         }
       });
-     }
+    }
     onMarkerSelect(args) { }
     onCameraChanged(args) { }
+    getMarkerFrom(lat: number, lng: number, title: string, snippet: string) : Marker {
+      let marker = new Marker();
+       marker.position = Position.positionFromLatLng(lat, lng);
+       marker.title = title;
+       marker.snippet = snippet;
+       marker.userData = { index : 1};
+       return marker;
+    }
 }
