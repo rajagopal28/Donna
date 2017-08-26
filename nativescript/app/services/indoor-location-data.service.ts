1fd7e6f920563cf2b320602f682c2156804bcd81
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-26 10:31:04 +0530

    Fixing decoding the polyline path string util method

--- nativescript/app/services/indoor-location-data.service.ts
+++ nativescript/app/services/indoor-location-data.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http } from "@angular/http";
+import { Color } from "tns-core-modules/color";
+
 import "rxjs/Rx";
 
 import { HotSpot } from '../models/app.models';
 
-var mapsModule = require("nativescript-google-maps-sdk");
+import { Polyline, Position } from "nativescript-google-maps-sdk";
 
 
 const URL : string = 'https://maps.googleapis.com/maps/api/directions/json';
@@ -26,21 +28,17 @@ export class IndoorLocationDataService {
     return points;
   }
   getPolyLineForWayPoint(waypoint) {
-    return new mapsModule.Position.positionFromLatLng(waypoint.lat, waypoint.lng);
+    return Position.positionFromLatLng(waypoint.lat, waypoint.lng);
   }
   getDirectionWayPointsAndLoadPolyLineOptions(params, cb) {
-    var p = {
-    	"origin" : "Adelaide,SA",
-    	"destination" : "Adelaide,SA",
-    	"waypoints" : "optimize:true|Barossa+Valley,SA|Clare,SA|Connawarra,SA|McLaren+Vale,SA",
-    	"key" : DIRECTIONS_API_KEY
-    };
     console.log('sending params to api...');
-    this.getDirectionWayPoints(p, (response) => {
+    params["key"] = DIRECTIONS_API_KEY;
+    this.getDirectionWayPoints(params, (response) => {
       let routes = response.routes;
       if(routes && routes.length > 0) {
         console.log('got routes', routes.length);
         let decodedPathList = this.decodePathLineString(routes[0].overview_polyline.points);
+        console.log('the polyline endoded str', routes[0].overview_polyline.points);
         console.log('decoded path list =', decodedPathList.length);
         let polyLineLocations = this.getPolyLineFromWayPoints(decodedPathList);
         console.log('got locations from string=', polyLineLocations.length);
@@ -51,22 +49,33 @@ export class IndoorLocationDataService {
       }
     });
   }
-  getPolylineOptionsFromPostions(polylinePositions: Array<Position>) {
-      let polyLineOptions = new mapsModule.Polyline();
+  getPolylineOptionsFromPostions(polylinePositions: Array<Position>) : Polyline {
+      let polyLineOptions : Polyline = new Polyline();
       console.log('android enna??', polyLineOptions);
       console.log('creating polyling options, length=', polylinePositions.length);
       polyLineOptions.width = 25;
       polyLineOptions.visible = true;
       polyLineOptions.geodesic = true;
-      polyLineOptions._points = polylinePositions;
-      console.log('before setting polyling options, adding points');
-        try {
-            polyLineOptions.loadPoints();
-        } catch(e) {
-          console.error(e);
-        }
-      console.log('after setting polyling options, adding points');
+      polyLineOptions.color = new Color("#FF0000");
+      polyLineOptions.addPoints(polylinePositions);
       return polyLineOptions;
+  }
+  getRouteText(params, cb) {
+    params["key"] = DIRECTIONS_API_KEY;
+    this.getDirectionWayPoints(params, (response) =>{
+        var restext = '';
+        if(response.routes && response.routes.length >0) {
+          var legsO = response.routes[0].legs;
+          for(var leg =0; leg < legsO.length; leg++ ) {
+            var legO = legsO[leg];
+            var steps = legO.steps;
+            for(var step = 0; step < steps.length; step++) {
+              restext += steps[step].html_instructions;
+            }
+          }
+        }
+        cb(restext);
+    });
   }
   getDirectionWayPoints(params, cb) {
     this.getDirectionwayPointsAPI(params)
@@ -76,37 +85,46 @@ export class IndoorLocationDataService {
         console.log("ERROR: ", error);
     });
   }
-  decodePathLineString(encodedPath) {
-    let len = encodedPath.length;
-    var index = 0;
-    var lat = 0;
-    var lng = 0;
+  decodePathLineString(encoded) {
+        if (!encoded) {
+            return [];
+        }
+        var poly = [];
+        var index = 0, len = encoded.length;
+        var lat = 0, lng = 0;
+        // For speed we preallocate to an upper bound on the final length, then
+        // truncate the array before returning.
+        while (index < len) {
+            var b, shift = 0, result = 0;
 
-     // For speed we preallocate to an upper bound on the final length, then
-     // truncate the array before returning.
-    let path = [];
-     while (index < len) {
-       var result = 1;
-       var shift = 0;
-       var b;
-       do {
-           b = encodedPath.charAt(index++) - 63 - 1;
-           result += b << shift;
-           shift += 5;
-       } while (b >= 0x1f);
-       lat += (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
-       result = 1;
-       shift = 0;
-       do {
-           b = encodedPath.charAt(index++) - 63 - 1;
-           result += b << shift;
-           shift += 5;
-       } while (b >= 0x1f);
-       lng += (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+            do {
+                b = encoded.charCodeAt(index++) - 63;
+                result = result | ((b & 0x1f) << shift);
+                shift += 5;
+            } while (b >= 0x20);
 
-       path.push({ lat: lat * 1e-5, lng: lng * 1e-5});
-     }
-     return path;
+            var dlat = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+
+            do {
+                b = encoded.charCodeAt(index++) - 63;
+                result = result | ((b & 0x1f) << shift);
+                shift += 5;
+            } while (b >= 0x20);
+
+            var dlng = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+            lng += dlng;
+
+            var p = {
+                lat: lat / 1e5,
+                lng: lng / 1e5,
+            };
+            poly.push(p);
+        }
+        return poly;
   }
 
   getDirectionwayPointsAPI(params) {
