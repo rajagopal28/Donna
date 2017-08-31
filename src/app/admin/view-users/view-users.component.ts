f2dde9003961feb112cc46c31cc1494c6101b009
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-31 23:30:33 +0530

    Adding view users actual updates with list view

--- src/app/admin/view-users/view-users.component.ts
+++ src/app/admin/view-users/view-users.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/app.models';
+import { UserDataService } from '../../services/user-data.service';
+
 @Component({
   selector: 'app-view-users',
   templateUrl: './view-users.component.html',
-  styleUrls: ['./view-users.component.css']
+  styleUrls: ['./view-users.component.css'],
+  providers: [UserDataService],
 })
 export class ViewUsersComponent implements OnInit {
+  users: [User];
 
-  constructor() { }
+  constructor(private userService: UserDataService) { }
 
   ngOnInit() {
+    this.userService.getAllUsers()
+    .subscribe(response=> this.users = response.items,
+     error => console.log(error),
+     () => console.log('Completed!!')
+    );
   }
 
 }
