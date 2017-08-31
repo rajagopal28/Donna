d46cf795f93d0e07c2e05f9c2b5f4ee821eb372c
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-31 23:30:33 +0530

    Updating signup page with actual controller actions, validation VIP

--- src/app/users/signup/signup.component.ts
+++ src/app/users/signup/signup.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User, Location } from '../../models/app.models';
+import { UserDataService } from '../../services/user-data.service';
+import { LocationDataService } from '../../services/location-data.service';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+  providers: [UserDataService, LocationDataService]
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  user : User;
+  locations: [Location];
+
+  constructor(private userService: UserDataService, private locationService: LocationDataService) { }
 
   ngOnInit() {
+    this.user = new User();
+    this.locationService.getAllLocations().subscribe(
+      response => this.locations = response.items,
+      error => console.log('Error:', error),
+      () => console.log('Completed..')
+    );
+  }
+
+  signUp(): void {
+    console.log('user', this.user);
+    this.userService.addUser(this.user).subscribe(
+      response => console.log(response),
+      error => console.log(error),
+      () => console.log('COmpleted!')
+    );
   }
 
 }
