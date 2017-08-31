6bfa61d2bbdccda5c7238d6e6dd5d5840efe8582
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-31 23:30:33 +0530

    Updated view and added logic to fetch all locations

--- src/app/admin/view-locations/view-locations.component.ts
+++ src/app/admin/view-locations/view-locations.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import {LocationDataService} from '../../services/location-data.service'
+import {Location} from '../../models/app.models'
 @Component({
   selector: 'app-view-locations',
   templateUrl: './view-locations.component.html',
-  styleUrls: ['./view-locations.component.css']
+  styleUrls: ['./view-locations.component.css'],
+  providers: [LocationDataService]
 })
 export class ViewLocationsComponent implements OnInit {
 
-  constructor() { }
+  locations: [Location];
+  constructor(private locationService: LocationDataService) {
+   }
 
   ngOnInit() {
+    this.locationService.getAllLocations().subscribe(
+      response => this.locations = response.items,
+      error => console.log(error),
+      () => console.log('COmpleted!')
+    );
   }
 
 }
