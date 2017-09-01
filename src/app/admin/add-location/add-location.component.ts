97d8892091d932d8e7a5f03636e408be100fe352
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-09-01 14:34:06 +0530

    Updating add-location component and adding it to tab

--- src/app/admin/add-location/add-location.component.ts
+++ src/app/admin/add-location/add-location.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Campus, Location } from '../../models/app.models';
+import {LocationDataService} from '../../services/location-data.service'
 @Component({
-  selector: 'app-add-location',
+  selector: 'AddLocation',
   templateUrl: './add-location.component.html',
-  styleUrls: ['./add-location.component.css']
+  styleUrls: ['./add-location.component.css'],
+  providers: [LocationDataService]
 })
 export class AddLocationComponent implements OnInit {
 
-  constructor() { }
+  campus : [Campus];
+  location: Location = new Location();
+  constructor(private locationService: LocationDataService) { }
 
   ngOnInit() {
+      this.locationService.getAllCampus().subscribe(
+          response => this.campus = response.items,
+          error => console.log(error),
+          () => console.log('C0mpleted!')
+        );
+  }
+  addLocation() {
+    console.log(this.location);
+    this.locationService.addLocation(this.location).subscribe(
+        response => console.log(response),
+        error => console.log(error),
+        () => console.log('C0mpleted!')
+      );
   }
 
 }
