311eca93765892c78557079949aaf0f468c9c291
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-09-01 23:20:45 +0530

    Updating add campus component to consume service

--- src/app/admin/add-campus/add-campus.component.ts
+++ src/app/admin/add-campus/add-campus.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Campus } from '../../models/app.models';
+import {LocationDataService} from '../../services/location-data.service';
 @Component({
-  selector: 'app-add-campus',
+  selector: 'AddCampus',
   templateUrl: './add-campus.component.html',
-  styleUrls: ['./add-campus.component.css']
+  styleUrls: ['./add-campus.component.css'],
+  providers: [LocationDataService]
 })
 export class AddCampusComponent implements OnInit {
-
-  constructor() { }
+  campus : Campus = new Campus();
+  constructor(private locationService: LocationDataService) { }
 
   ngOnInit() {
   }
 
+  addCampus() {
+    console.log(this.campus);
+    this.locationService.addCampus(this.campus).subscribe(
+        response => console.log(response),
+        error => console.log(error),
+        () => console.log('C0mpleted!')
+      );
+  }
 }
