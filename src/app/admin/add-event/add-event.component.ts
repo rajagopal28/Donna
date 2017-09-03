a3701dbdb5d083ce3f2213cc17f4189c415557ce
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-09-03 21:57:37 +0530

    Adding event participant add logic and updating UI with lot of component changes

--- src/app/admin/add-event/add-event.component.ts
+++ src/app/admin/add-event/add-event.component.ts
@@ -1,15 +1,98 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/first';
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 
+import { User, Location, Event } from '../../models/app.models';
+import { UserDataService } from '../../services/user-data.service';
+import { LocationDataService } from '../../services/location-data.service';
+import { ChoresDataService } from '../../services/chores-data.service';
 @Component({
-  selector: 'app-add-event',
+  selector: 'AddEvent',
   templateUrl: './add-event.component.html',
-  styleUrls: ['./add-event.component.css']
+  styleUrls: ['./add-event.component.css'],
+  providers: [UserDataService, LocationDataService, ChoresDataService]
 })
 export class AddEventComponent implements OnInit {
+  event: Event;
+  locations: [Location];
+  users: [User];
+  selectedUsers: [User];
+  model: NgbDateStruct;
 
-  constructor() { }
+  startDate: NgbDateStruct;
+  sDate: { year: number, month: number, day: number };
+
+  endDate: NgbDateStruct;
+  eDate: { year: number, month: number, day: number};
+
+    startTime : {hour: 08, minute: 0};
+    endTime : {hour: 08, minute: 0};
+
+  constructor(private userService: UserDataService, private locationService: LocationDataService, private choresService: ChoresDataService) { }
 
   ngOnInit() {
+    this.event= new Event();
+    this.userService.getAllUsers()
+      .subscribe(response=> this.users = response.items,
+       error => console.log(error),
+       () => console.log('Completed!!')
+      );
+    this.locationService.getAllLocations().subscribe(
+      response => this.locations = response.items,
+      error => console.log('Error:', error),
+      () => console.log('Completed..')
+    );
+  }
+  addEvent() {
+    console.log(this.selectedUsers);
+    let participantIds = '';
+    for(var i=0;i<this.selectedUsers.length; i++) {
+      if (participantIds !== '') {
+        participantIds += ',';
+      }
+      participantIds += this.selectedUsers[i].id;
+    }
+    console.log('csv', participantIds);
+    this.event.participantIds = participantIds;
+    var teDate = new Date(this.startDate.year,this.startDate.month,this.startDate.day,this.startTime.hour,this.startTime.minute,0);
+    this.event.eventStart = teDate.getTime();
+
+    teDate = new Date(this.endDate.year,this.endDate.month,this.endDate.day,this.endTime.hour,this.endTime.minute,0);
+    this.event.eventEnd = teDate.getTime();
+    console.log(this.event);
+    this.choresService.addEvent(this.event).subscribe(
+      response => console.log(response),
+      error => console.log(error),
+      () => console.log("C0pLeted!")
+    );
   }
 
+  public onRemove = ($event: User) : void => {
+    console.log('Removing items...', $event);
+    if (!this.selectedUsers) {
+      return;
+    }
+
+    let index = this.selectedUsers.indexOf($event);
+    if(index !== -1) {
+      this.selectedUsers.splice(index, 1);
+    }
+    // console.log(index);
+    // console.log(this.);
+  };
+
+  public onAdd = ($event: User) : void => {
+    console.log('Adding items...', $event);
+    if (this.selectedUsers) {
+      this.selectedUsers.push($event);
+    } else {
+      this.selectedUsers =[$event];
+    }
+    // console.log(this.items);
+  }
 }
