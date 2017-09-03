b9a5cbfa3052e799ff202e73cb0025b18d221220
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-09-03 19:11:49 +0530

    Adding add event and announcement end points

--- src/app/services/chores-data.service.ts
+++ src/app/services/chores-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
 
+import { Announcement, Event } from '../models/app.models';
 import {BaseAPIDataService} from  './base-api-data.service'
 
 @Injectable()
@@ -11,8 +12,13 @@ export class ChoresDataService extends BaseAPIDataService {
   getAllAnnouncements(){
     return super.getData('announcements', {});
   }
-
+  addAnnouncement(announcement : Announcement) {
+    return super.postData('announcements', announcement);
+  }
    getEvents(){
      return super.getData('events', {});
+   }
+   addEvent(event : Event) {
+     return super.postData('events', event);
    }
 }
