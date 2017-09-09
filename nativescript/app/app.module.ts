19ec5ef746df48c42398c4044d21188addbf62e2
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-09-09 17:40:57 +0530

    Adding routes to tabbed views

--- nativescript/app/app.module.ts
+++ nativescript/app/app.module.ts
@@ -3,21 +3,34 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { ModalDialogService } from "nativescript-angular/modal-dialog";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 import { AppComponent } from "./app.component";
+import { TabbedHomeComponent } from "./tabbed-home/tabbed-home.component";
+import { TabbedDroneComponent } from "./tabbed-drone/tabbed-drone.component";
 import { HomeComponent } from './home/home.component';
 import { ChatBotComponent } from './chat-bot/chat-bot.component';
 import { IndoorMapComponent } from './indoor-map/indoor-map.component';
 import { ViewAnnouncementsComponent } from './view-announcements/view-announcements.component';
 import { LoginModalComponent } from './login-modal/login-modal.component';
 
+export const routes = [
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "home", component: TabbedHomeComponent },
+    { path: "drone", component: TabbedDroneComponent }
+];
+
 @NgModule({
   declarations: [AppComponent, HomeComponent,
      ChatBotComponent, IndoorMapComponent,
-     ViewAnnouncementsComponent, LoginModalComponent],
+     ViewAnnouncementsComponent, LoginModalComponent,
+    TabbedHomeComponent, TabbedDroneComponent],
   entryComponents: [LoginModalComponent],
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, NativeScriptHttpModule, NativeScriptFormsModule],
+  imports: [NativeScriptModule, NativeScriptHttpModule,
+        NativeScriptFormsModule,
+        NativeScriptRouterModule,
+        NativeScriptRouterModule.forRoot(routes)],
   schemas: [NO_ERRORS_SCHEMA],
   providers: [ModalDialogService]
 })
