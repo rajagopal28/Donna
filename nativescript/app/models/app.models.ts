e6d9ec4b3f1e6c70b0004a29d5f359ce19675fc8
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-09-09 12:08:04 +0530

    Including all models from web

--- nativescript/app/models/app.models.ts
+++ nativescript/app/models/app.models.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-export class Location {
+export class ILocation {
    lat: number;
    lng: number;
    z: number;
@@ -8,7 +8,28 @@ export class HotSpot {
   id: number;
   name: String;
   building: String;
-  location: Location;
+  location: ILocation;
+}
+
+
+
+ export class Location {
+   id: number;
+   latitude: number;
+   longitude: number;
+   name: string;
+   campusId: number;
+   campus: Campus;
+   floor: number;
+}
+
+export class Campus {
+  latitude: number;
+  longitude: number;
+  name: string;
+  id: number;
+  campusNumber : number;
+  locations : [Location];
 }
 
 export class ChatItem {
@@ -16,5 +37,47 @@ export class ChatItem {
   content: string;
   isSelf: boolean;
   user : string;
+  meta: any;
   timestamp : Date
+}
+
+
+export class User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  createdTS: number;
+  lastUpdatedTS : number;
+  username : string;
+  password: string;
+  locationId: number;
+  location: Location;
+}
+
+export class EventParticipant {
+  id: number;
+  eventId: number;
+  participantId: number;
+  participant: User;
+}
+
+export class Event {
+  title: string;
+  description: string;
+  eventStart: number;
+  eventEnd: number;
+  locationId: number;
+  id: number;
+  participants: [EventParticipant];
+  participantIds: string;
+}
+
+
+export class Announcement {
+  title: string;
+  description: string;
+  validFrom: number;
+  validTill: number;
+  category: string;
+  id: number;
 }
