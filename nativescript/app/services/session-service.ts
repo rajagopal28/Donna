9512ac80bc92bc15083fa3cc0af269c1ee63acdc
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-09-10 13:01:29 +0530

    Adding application setting to save user and reset on logout

--- nativescript/app/services/session-service.ts
+++ nativescript/app/services/session-service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core'
+import { Injectable } from '@angular/core';
+import {
+    getString,
+    setString,
+    hasKey,
+    remove
+} from "application-settings";
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 import { User } from '../models/app.models';
 
+const AUTH_USER_KEY = 'autUserString';
 @Injectable()
 export class SessionService {
   private _authSetting = new BehaviorSubject<User>(null);
@@ -11,6 +18,7 @@ export class SessionService {
   public authObervable = this._authSetting.asObservable();
   constructor() {
     this.clearUser();
+    this.getAuthFromPreferences();
   }
   getUser() : User {
     return this.authenticatedUser;
@@ -18,9 +26,19 @@ export class SessionService {
   setUser(user: User) {
     this.authenticatedUser = user;
     this._authSetting.next(user);
+    if(user) {
+      setString(AUTH_USER_KEY, JSON.stringify(user))
+    } else {
+      remove(AUTH_USER_KEY);
+    }
   }
   isLoggedIn() : boolean {
     return this.authenticatedUser !== null;
+  }
+  getAuthFromPreferences() {
+    if(hasKey(AUTH_USER_KEY)) {
+      this.setUser(JSON.parse(getString(AUTH_USER_KEY)));
+    }
   }
   clearUser() {
     this.setUser(null);
