234042a7eca457bfc5a88402a7756636b484dd2e
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-09-13 11:36:09 +0530

    Adding response handling in campus and location add

--- src/app/admin/add-campus/add-campus.component.ts
+++ src/app/admin/add-campus/add-campus.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 import { Campus } from '../../models/app.models';
 import {LocationDataService} from '../../services/location-data.service';
+
+const ALERT_TYPE_ERROR = 'danger';
+const ALERT_TYPE_SUCCESS = 'success';
+const DEFAULT_SUCCESS_MESSAGE = 'Insertion Successful!';
+
 @Component({
   selector: 'AddCampus',
   templateUrl: './add-campus.component.html',
@@ -9,6 +14,13 @@ import {LocationDataService} from '../../services/location-data.service';
   providers: [LocationDataService]
 })
 export class AddCampusComponent implements OnInit {
+
+    message : any = {
+      content : '',
+      isHidden: true,
+      type: ALERT_TYPE_ERROR
+    };
+
   campus : Campus = new Campus();
   constructor(private locationService: LocationDataService) { }
 
@@ -18,9 +30,22 @@ export class AddCampusComponent implements OnInit {
   addCampus() {
     console.log(this.campus);
     this.locationService.addCampus(this.campus).subscribe(
-        response => console.log(response),
-        error => console.log(error),
+        response => this.handleResponse(response),
+        error =>  this.handleResponse(error),
         () => console.log('C0mpleted!')
       );
   }
+  handleResponse(response){
+     console.log(response);
+     this.message.isHidden = false;
+     if(response.success) {
+       this.campus= new Campus();
+       this.message.content = DEFAULT_SUCCESS_MESSAGE;
+       this.message.type = ALERT_TYPE_SUCCESS;
+     } else  {
+       this.message.content = response.message? response.message : 'ERROR:' + JSON.stringify(response);
+       this.message.type = ALERT_TYPE_ERROR;
+     }
+  }
+
 }
--- src/app/admin/add-location/add-location.component.ts
+++ src/app/admin/add-location/add-location.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 import { Campus, Location } from '../../models/app.models';
 import {LocationDataService} from '../../services/location-data.service';
+
+const ALERT_TYPE_ERROR = 'danger';
+const ALERT_TYPE_SUCCESS = 'success';
+const DEFAULT_SUCCESS_MESSAGE = 'Insertion Successful!';
+
 @Component({
   selector: 'AddLocation',
   templateUrl: './add-location.component.html',
@@ -9,7 +14,11 @@ import {LocationDataService} from '../../services/location-data.service';
   providers: [LocationDataService]
 })
 export class AddLocationComponent implements OnInit {
-
+  message : any = {
+    content : '',
+    isHidden: true,
+    type: ALERT_TYPE_ERROR
+  };
   campus : [Campus];
   location: Location = new Location();
   constructor(private locationService: LocationDataService) { }
@@ -24,10 +33,23 @@ export class AddLocationComponent implements OnInit {
   addLocation() {
     console.log(this.location);
     this.locationService.addLocation(this.location).subscribe(
-        response => console.log(response),
-        error => console.log(error),
+        response =>  this.handleResponse(response),
+        error => this.handleResponse(error),
         () => console.log('C0mpleted!')
       );
+  }
+
+  handleResponse(response){
+     console.log(response);
+     this.message.isHidden = false;
+     if(response.success) {
+       this.location= new Location();
+       this.message.content = DEFAULT_SUCCESS_MESSAGE;
+       this.message.type = ALERT_TYPE_SUCCESS;
+     } else  {
+       this.message.content = response.message? response.message : 'ERROR:' + JSON.stringify(response);
+       this.message.type = ALERT_TYPE_ERROR;
+     }
   }
 
 }
