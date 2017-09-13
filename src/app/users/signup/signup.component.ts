f384a8bb2839477d895ae1da78984e18ec6ee94e
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-09-13 11:35:39 +0530

    Adding alert in signup page

--- src/app/users/signup/signup.component.ts
+++ src/app/users/signup/signup.component.ts
@@ -4,6 +4,10 @@ import { User, Location } from '../../models/app.models';
 import { UserDataService } from '../../services/user-data.service';
 import { LocationDataService } from '../../services/location-data.service';
 
+const ALERT_TYPE_ERROR = 'danger';
+const ALERT_TYPE_SUCCESS = 'success';
+const DEFAULT_SUCCESS_MESSAGE = 'Insertion Successful!';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -11,13 +15,16 @@ import { LocationDataService } from '../../services/location-data.service';
   providers: [UserDataService, LocationDataService]
 })
 export class SignupComponent implements OnInit {
-
+  message : any = {
+    content : '',
+    isHidden: true,
+    type: ALERT_TYPE_ERROR
+  };
   user : User;
   confirmPassword: string;
   locations: [Location];
 
   constructor(private userService: UserDataService, private locationService: LocationDataService) { }
-
   ngOnInit() {
     this.user = new User();
     this.locationService.getAllLocations({}).subscribe(
@@ -30,10 +37,22 @@ export class SignupComponent implements OnInit {
   signUp(): void {
     console.log('user', this.user);
     this.userService.addUser(this.user).subscribe(
-      response => console.log(response),
-      error => console.log(error),
+      response => this.handleResponse(response),
+      error =>  this.handleResponse(error),
       () => console.log('COmpleted!')
     );
+  }
+  handleResponse(response){
+     console.log(response);
+     this.message.isHidden = false;
+     if(response.success) {
+       this.user= new User();
+       this.message.content = DEFAULT_SUCCESS_MESSAGE;
+       this.message.type = ALERT_TYPE_SUCCESS;
+     } else  {
+       this.message.content = response.message? response.message : 'ERROR:' + JSON.stringify(response);
+       this.message.type = ALERT_TYPE_ERROR;
+     }
   }
 
 }
